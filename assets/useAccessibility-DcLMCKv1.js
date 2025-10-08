dea032d6e9af3c8b6c857c5df46f3cf46c95ff2f
biswarup96 <biswarana96.rana@gmail.com>
2025-10-08 16:24:45 +0530

    target is increased upto 12 digits

---
+++ assets/useAccessibility-DcLMCKv1.js
@@ -0,0 +1,1 @@
+import{u}from"./index-Ba8ks1Kx.js";import{l as e}from"./lodash-BgPpO2vf.js";function a(){const s=u(t=>t.auth.user.authority)||[];return{canAccess:t=>{const r=[t];return e.isEmpty(r)||e.isEmpty(s)?!0:r.some(o=>s.includes(o))}}}export{a as u};
